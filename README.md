5efa119a8262953286d26151d4934b6975ae5eff
shockham <samuel.hockham@gmail.com>
2017-05-07 14:47:08 +0100

    Add crate documentation and fill out the README a bit more

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
 # supervisor
 
 Spawns a thread and which will restart the work fn when it produces a Result.
+
+[Example usage](https://github.com/mount-research/supervisor/blob/master/examples/simple.rs):
+```rust
+extern crate supervisor;
+
+fn main() {
+    supervisor::supervise(move || -> Result<&'static str, &'static str> {
+        for i in 0 .. 5 {
+            println!("trying: {}", i);
+            if i == 4 {
+                return Err("failed work reason");
+            }
+        }
+        Ok("finished")
+    });
+
+    // run an inf loop whilst supervised thread does work
+    loop {
+        std::thread::sleep(std::time::Duration::new(1u64, 0u32));
+    }
+}
+```
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,34 @@
+/*!
+
+Spawns a thread and which will restart the work fn when it produces a Result.
+
+[Example usage](https://github.com/mount-research/supervisor/blob/master/examples/simple.rs):
+
+```
+extern crate supervisor;
+
+fn main() {
+    supervisor::supervise(move || -> Result<&'static str, &'static str> {
+        for i in 0 .. 5 {
+            println!("trying: {}", i);
+            if i == 4 {
+                return Err("failed work reason");
+            }
+        }
+        Ok("finished")
+    });
+
+    // run an inf loop whilst supervised thread does work
+    loop {
+        std::thread::sleep(std::time::Duration::new(1u64, 0u32));
+    }
+}
+```
+
+*/
+
+#![deny(missing_docs)]
+
 #[macro_use] extern crate log;
 
 use std::thread;
